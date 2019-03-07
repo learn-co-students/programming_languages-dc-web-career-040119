7b54d1e42e3c106691dba3122db45afceb9e7516
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-07 01:59:22 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,7 @@
 def reformat_languages(languages)
-  # your code here
+ new_hash = {}
+ new_hash = :ruby
+ new_hash[:ruby] = {:type => "interpreted"}
+ new_hash[:ruby] = {:style => [:00]}  # your code here
+end
 end
