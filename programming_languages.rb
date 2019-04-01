7dd942437de160d76076997f28133b40cc4b5ae1
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-01 06:42:03 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,27 @@
+require 'pry'
 def reformat_languages(languages)
   # your code here
+  n_hash= {}
+  jscript= []
+  languages.each do |style, data|
+     
+    data.each do |langs, value|
+      value.each do |typ, val|
+          
+        if langs == :javascript
+          jscript<< style
+          if jscript.length > 1
+          n_hash[langs]= {typ => val, :style => jscript}
+        end
+        elsif langs != :javascript
+           n_hash[langs]= {typ => val, :style => [style]}
+        else
+          puts 'Nothing'
+        end
+      
+      end
+    end
+  end
+  n_hash
+ #binding.pry
 end
